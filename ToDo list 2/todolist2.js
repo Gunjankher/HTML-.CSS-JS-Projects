728528e9f685b0ed27b80fc79e7d7b3e62bec4ee
Gunjan Kher <147532045+Gunjankher@users.noreply.github.com>
2024-02-03 16:10:22 +0530

    Add files via upload

---
+++ ToDo list 2/todolist2.js
@@ -0,0 +1,23 @@
+let input = document.getElementById("inp")
+let text  = document.querySelector(".text")
+
+function Add(){
+    if(input.value===""){
+        alert("please Enter a task")
+    }
+    else {
+       let newEle = document.createElement("ul")
+        newEle.innerHTML = `${input.value}<i class="fa-solid fa-trash"></i>`
+        text.appendChild(newEle)
+        input.value= ""
+        newEle.querySelector("i").addEventListener("click", remove)
+        function remove(){
+            newEle.remove()
+        }
+
+    }
+}
+
+
+
+
